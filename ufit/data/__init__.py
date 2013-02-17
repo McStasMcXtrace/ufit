c4bbbe4d3a62d589819062dad8334230b290cb3e
Georg Brandl <georg@python.org>
2013-02-17 23:16:57 +0100

    data: add docstrings

--- ufit/data/__init__.py
+++ ufit/data/__init__.py
@@ -28,16 +28,51 @@ __all__ = ['Dataset', 'sets', 'set_datatemplate', 'set_dataformat',
 global_loader = Loader()
 sets = global_loader.sets
 
-def set_datatemplate(s):
-    global_loader.template = s
+def set_datatemplate(template):
+    """Set a new template for data file names.
 
-def set_dataformat(s):
-    if s not in data_formats:
-        raise UFitError('Unknown data format %r' % s)
-    global_loader.format = s
+    Normally, data file names consist of a fixed part and a sequential number.
+    Therefore ufit constructs file names from a data template, which should
+    contain a placeholder like ``%06d`` (for a 6-digit sequential number), and
+    the actual file number given in the :func:`read_data` function.
 
-def read_data(*args):
-    return global_loader.load(*args)
+    An example::
+
+       set_datatemplate('/data/exp/data2012n%06d.dat')
+       d1 = read_data(100)
+       d2 = read_data(101)
+       # etc.
+    """
+    global_loader.template = template
+
+def set_dataformat(format):
+    """Set the input data format.
+
+    Normally ufit autodetects file formats, but this can be overridden using
+    this function.  Data formats are:
+
+    * ``'ill'`` - ILL TAS data format
+    * ``'nicos'`` - NICOS data format
+    * ``'simple'`` - simple whitespace-separated multi-column files
+    """
+    if format not in data_formats:
+        raise UFitError('Unknown data format %r' % format)
+    global_loader.format = format
+
+def read_data(n, xcol, ycol, dycol=None, ncol=None, nscale=1):
+    """Read a data file.  Returns a :class:`Dataset` object.
+
+    :param xcol: X column name (or 1-based index)
+    :param ycol: Y column name (or 1-based index)
+    :param ycol: Y errors column name (or 1-based index); the default is to take
+        the square root of the Y column as appropriate for counts
+    :param ncol: normalization column name (or 1-based index); typically a beam
+        monitor column
+    :param nscale: scale for the normalization column; the Y data is determined
+        as ``y[i] = y_raw[i] / ncol[i] * nscale``
+    """
+    return global_loader.load(n, xcol, ycol, dycol, ncol, nscale)
 
 def as_data(x, y, dy, name=''):
+    """Quickly construct a :class:`Dataset` object from three numpy arrays."""
     return Dataset.from_arrays(name or 'data', x, y, dy)
