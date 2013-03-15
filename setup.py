01df85586c488e3a2864ac0134c13378532f5545
Georg Brandl <georg@python.org>
2013-03-15 16:15:36 +0100

    version update

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def find_ui_files():
 
 setup(
     name = 'ufit',
-    version = '0.99',
+    version = '0.99.1',
     license = 'GPL',
     author = 'Georg Brandl',
     author_email = 'georg.brandl@frm2.tum.de',
