ae350ffd1fed43dce31fb0e92196c2341bd8d545
Georg Brandl <georg@python.org>
2013-05-10 18:06:26 +0200

    bump to 0.99.6

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def find_ui_files():
 
 setup(
     name = 'ufit',
-    version = '0.99.5',
+    version = '0.99.6',
     license = 'GPL',
     author = 'Georg Brandl',
     author_email = 'georg.brandl@frm2.tum.de',
