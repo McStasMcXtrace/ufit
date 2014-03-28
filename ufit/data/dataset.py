aa151add465fc72e82cd93482d8b74ef83edb190
Petr Cermak <p.cermak@fz-juelich.de>
2014-03-28 14:39:08 +0100

    Fix of dataset merging When two datasets have different columns, it will produce error (like two file supplied from PUMA, one has time column and the another has timer column (I have no idea why!)). This will allow to merge these datasets together.

--- ufit/data/dataset.py
+++ ufit/data/dataset.py
@@ -166,7 +166,7 @@ class Dataset(object):
         for col in self.meta:
             if not col.startswith('col_'):
                 continue
-            new_meta[col] = concatenate([dset.meta[col] for dset in allsets])
+            new_meta[col] = concatenate([dset.meta.get(col, []) for dset in allsets])
         # XXX hkl data is a mess
         if 'is_hkldata' in self.meta:
             concat = concatenate([dset.meta['hkle'] for dset in allsets])
