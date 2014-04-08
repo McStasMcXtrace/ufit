21b62e81d84dd8c2759bc8cc78ab8303cb9775c0
Georg Brandl <georg@python.org>
2014-04-08 21:26:16 +0200

    gui: add a logfile logging handler that logs warnings and above

--- ufit/gui/loggers.py
+++ ufit/gui/loggers.py
@@ -6,11 +6,15 @@
 # Licensed under a 2-clause BSD license, see LICENSE.
 # *****************************************************************************
 
-"""Logging utilities for ufit."""
+"""Logging utilities for ufit.
+
+Mostly borrowed from nicos.utils.loggers.
+"""
 
 import os
 import time
 import traceback
+from os import path
 from logging import Formatter, StreamHandler, DEBUG, INFO, WARNING, root, \
     getLogger
 
@@ -91,7 +95,7 @@ class ConsoleFormatter(Formatter):
         levelno = record.levelno
         datefmt = self.colorize('lightgray', '[%(asctime)s] ')
         record.message = record.getMessage()
-        if record.name == 'nicos':
+        if record.name == 'ufit':
             namefmt = ''
         else:
             namefmt = '%(name)s: '
@@ -114,8 +118,106 @@ class ConsoleFormatter(Formatter):
             s += '\n' + self.formatException(record.exc_info).rstrip()
         return s
 
+
+LOGFMT = '%(asctime)s : %(levelname)-7s : %(name)s: %(message)s'
+DATESTAMP_FMT = '%Y-%m-%d'
+SECONDS_PER_DAY = 60 * 60 * 24
+
+class LogfileHandler(StreamHandler):
+    """
+    Logs to log files with a date stamp appended, and rollover on midnight.
+    """
+
+    def __init__(self, directory, filenameprefix='ufit', filenamesuffix=None,
+                 dayfmt=DATESTAMP_FMT):
+        if not path.isdir(directory):
+            os.makedirs(directory)
+        self._currentsymlink = path.join(directory, 'current')
+        self._filenameprefix = filenameprefix
+        self._filenamesuffix = filenamesuffix
+        self._pathnameprefix = path.join(directory, filenameprefix)
+        self._dayfmt = dayfmt
+        # today's logfile name
+        if filenamesuffix:
+            basefn = self._pathnameprefix + '-' + time.strftime(dayfmt) + \
+                '-' + filenamesuffix + '.log'
+        else:
+            basefn = self._pathnameprefix + '-' + time.strftime(dayfmt) + '.log'
+        self.baseFilename = path.abspath(basefn)
+        self.mode = 'a'
+        StreamHandler.__init__(self, self._open())
+        # determine time of first midnight from now on
+        t = time.localtime()
+        self.rollover_at = time.mktime((t[0], t[1], t[2], 0, 0, 0,
+                                        t[6], t[7], t[8])) + SECONDS_PER_DAY
+        self.setFormatter(Formatter(LOGFMT, DATEFMT))
+        self.disabled = False
+
+    def _open(self):
+        # update 'current' symlink upon open
+        try:
+            os.remove(self._currentsymlink)
+        except OSError:
+            # if the symlink does not (yet) exist, OSError is raised.
+            # should happen at most once per installation....
+            pass
+        if hasattr(os, 'symlink'):
+            os.symlink(path.basename(self.baseFilename), self._currentsymlink)
+        # finally open the new logfile....
+        return open(self.baseFilename, self.mode)
+
+    def filter(self, record):
+        return not self.disabled
+
+    def emit(self, record): #pylint: disable=W0221
+        try:
+            t = int(time.time())
+            if t >= self.rollover_at:
+                self.doRollover()
+            if self.stream is None:
+                self.stream = self._open()
+            StreamHandler.emit(self, record)
+        except Exception:
+            self.handleError(record)
+
+    def enable(self, enabled):
+        if enabled:
+            self.disabled = False
+            self.stream.close()
+            self.stream = self._open()
+        else:
+            self.disabled = True
+
+    def close(self):
+        self.acquire()
+        try:
+            if self.stream:
+                self.flush()
+                if hasattr(self.stream, 'close'):
+                    self.stream.close()
+                StreamHandler.close(self)
+                self.stream = None
+        finally:
+            self.release()
+
+    def doRollover(self):
+        self.stream.close()
+        if self._filenamesuffix:
+            self.baseFilename = '%s-%s-%s.log' % (
+                self._pathnameprefix, time.strftime(self._dayfmt),
+                self._filenamesuffix)
+        else:
+            self.baseFilename = self._pathnameprefix + '-' + \
+                time.strftime(self._dayfmt) + '.log'
+        self.stream = self._open()
+        self.rollover_at += SECONDS_PER_DAY
+
+
 handler = StreamHandler()
 fmter = ConsoleFormatter(colorize=colorize)
 handler.setFormatter(fmter)
 root.addHandler(handler)
+loghandler = LogfileHandler(path.expanduser('~/.config/ufit'))
+loghandler.setLevel(WARNING)
+root.addHandler(loghandler)
 root.setLevel(INFO)
